d905c167154f7e9f23ea7ec30621b94ad58f55f8
todd-bowers <toddbows@gmail.com>
2022-07-30 15:11:02 -0400

    done

--- src/main/java/io/zipcoder/Music.java
+++ src/main/java/io/zipcoder/Music.java
@@ -4,11 +4,33 @@ public class Music {
 
     private String[] playList;
 
-    public Music(String[] playList){
+    public Music(String[] playList) {
         this.playList = playList;
     }
 
-    public Integer selection(Integer startIndex, String selection){
-        return null;
+    public Integer selection(Integer startIndex, String selection) {
+        Integer skips = null;
+        Integer forwards = fastForward(startIndex, selection);
+        Integer backwards = reverse(startIndex, selection);
+        if (backwards > forwards) skips = forwards;
+        else skips = backwards;
+        return skips;
+    }
+
+    public Integer fastForward(Integer startIndex, String selection) {
+        Integer numOfSkips = 0;
+        for (int i = startIndex; !playList[i].contains(selection); i++) {
+            if (i == playList.length - 1) i -= playList.length;
+            numOfSkips++;
+        }
+        return numOfSkips;
+    }
+    public Integer reverse(Integer startIndex, String selection) {
+        Integer numOfSkips = 0;
+        for (int i = startIndex; !playList[i].contains(selection); i--) {
+            if (i == 0) i = playList.length;
+            numOfSkips++;
+        }
+        return numOfSkips;
     }
 }
